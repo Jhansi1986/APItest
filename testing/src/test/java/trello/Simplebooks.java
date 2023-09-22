ebbbed1fdba6986d9a85e40a4f0fcea2d810af96
jhans <jhans@DESKTOP-I75RT8S>
2023-09-22 19:54:50 +0530

    Simple books

--- testing/src/test/java/trello/Simplebooks.java
+++ testing/src/test/java/trello/Simplebooks.java
@@ -6,11 +6,15 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
+
 import static io.restassured.RestAssured.given;
 
 public class Simplebooks {
 	public static String BaseURL = "https://simple-books-api.glitch.me";
 	public static String Token;
+	public static String ID;
+	
+	
 	@Test(priority = 0)
 	public void Authentication()
 	
@@ -58,14 +62,78 @@ public class Simplebooks {
 				.then()
 				.assertThat().statusCode(201)
 				.extract().response();
+		JsonPath js = new JsonPath(respo.asString());
+		ID =js.get("id");
 		System.out.println(respo.asPrettyString());
-				
-					
-					
-					
 					
 		}
 		
+		@Test(priority = 2)
+		
+		public void  get_a_book()
+		{
+	      RestAssured.baseURI = BaseURL;
+	      
+	      Response respo = given()
+	    		  
+	    		  .queryParam("token", Token)
+	    		  .header("content-type","application/json")
+	    		  
+	    		  .when()
+	    		  .get("/books/"+ ID)
+	    		  
+	    		  .then()
+	    		  .assertThat().statusCode(200)
+	    		  
+	    		  .extract().response();
+	      
+	      System.out.println(respo.asPrettyString());
+		}
+		
+		@Test(priority = 3)
+		
+		public void update_a_order() {
+			RestAssured.baseURI = BaseURL;
+			
+			String requestbody="{\r\n"
+					+ "  \"customerName\": \"John\"\r\n"
+					+ "}";
+			
+			Response respo = given()
+			.queryParam("token", Token)
+			.header("content-Type","aapplication/json")
+			.body(requestbody)
+			
+			.when()
+			.patch("/orders/PF6MflPDcuhWobZcgmJy5")
+			
+			.then()
+			.assertThat().statusCode(200)
+			.extract().response();
+			
+			System.out.println(respo.asPrettyString());
+			
+			
+		}
+		
+		@Test(priority = 4)
+		
+		public void delete_a_board()
+		{
+			RestAssured.baseURI = BaseURL;
+			Response respo = given()
+			
+					.header("content-Type","application/json")
+					
+					.when()
+					.delete("/orders/"+ID)
+					
+					.then()
+					.assertThat().statusCode(200)
+					.extract().response();
+			System.out.println(respo.asPrettyString());
+			
+		}
 		
 		
 	
